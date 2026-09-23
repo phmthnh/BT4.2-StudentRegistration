9d851f0d47b0fce775973a5b9ab19ca6e4d5a77d
Phạm Tuấn Thành <tuanthanhpham206@gmail.com>
2026-09-23 16:26:07 +0700

    fix: remove gender (RadioButton) controls from RegistrationForm

--- RegistrationForm.Designer.cs
+++ RegistrationForm.Designer.cs
@@ -12,102 +12,90 @@ namespace BT4_2_StudentRegistration
 
         private void InitializeComponent()
         {
-            lblTitle        = new System.Windows.Forms.Label();
-            lblFullName     = new System.Windows.Forms.Label();
-            txtFullName     = new System.Windows.Forms.TextBox();
-            lblGender       = new System.Windows.Forms.Label();
-            rdoMale         = new System.Windows.Forms.RadioButton();
-            rdoFemale       = new System.Windows.Forms.RadioButton();
-            lblBirthDate    = new System.Windows.Forms.Label();
-            dtpBirthDate    = new System.Windows.Forms.DateTimePicker();
-            lblPhone        = new System.Windows.Forms.Label();
-            mtxtPhone       = new System.Windows.Forms.MaskedTextBox();
-            lblCourse       = new System.Windows.Forms.Label();
-            cboCourse       = new System.Windows.Forms.ComboBox();
-            lblExtras       = new System.Windows.Forms.Label();
-            chkEnglish      = new System.Windows.Forms.CheckBox();
-            chkSoftSkills   = new System.Windows.Forms.CheckBox();
-            chkIT           = new System.Windows.Forms.CheckBox();
-            btnRegister     = new System.Windows.Forms.Button();
-            btnClear        = new System.Windows.Forms.Button();
+            lblTitle      = new System.Windows.Forms.Label();
+            lblFullName   = new System.Windows.Forms.Label();
+            txtFullName   = new System.Windows.Forms.TextBox();
+            lblBirthDate  = new System.Windows.Forms.Label();
+            dtpBirthDate  = new System.Windows.Forms.DateTimePicker();
+            lblPhone      = new System.Windows.Forms.Label();
+            mtxtPhone     = new System.Windows.Forms.MaskedTextBox();
+            lblCourse     = new System.Windows.Forms.Label();
+            cboCourse     = new System.Windows.Forms.ComboBox();
+            lblExtras     = new System.Windows.Forms.Label();
+            chkEnglish    = new System.Windows.Forms.CheckBox();
+            chkSoftSkills = new System.Windows.Forms.CheckBox();
+            chkIT         = new System.Windows.Forms.CheckBox();
+            btnRegister   = new System.Windows.Forms.Button();
+            btnClear      = new System.Windows.Forms.Button();
             SuspendLayout();
 
-            // Form
-            Text          = "Đăng Ký Học Viên";
-            Size          = new System.Drawing.Size(480, 420);
-            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            // Form - bỏ giới tính nên giảm chiều cao
+            Text            = "Đăng Ký Học Viên";
+            Size            = new System.Drawing.Size(480, 370);
+            StartPosition   = System.Windows.Forms.FormStartPosition.CenterScreen;
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            MaximizeBox   = false;
+            MaximizeBox     = false;
 
-            // lblTitle
+            // Tiêu đề
             lblTitle.Text      = "FORM ĐĂNG KÝ HỌC VIÊN";
             lblTitle.Font      = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold);
             lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             lblTitle.Location  = new System.Drawing.Point(50, 15);
             lblTitle.Size      = new System.Drawing.Size(370, 32);
 
-            int labelX = 30, controlX = 170, rowH = 40;
-            int row = 60;
+            int lx = 30, cx = 170, rh = 40, row = 60;
 
-            // Họ Tên
+            // Họ và tên
             lblFullName.Text     = "Họ và tên:";
-            lblFullName.Location = new System.Drawing.Point(labelX, row + 3);
+            lblFullName.Location = new System.Drawing.Point(lx, row + 3);
             lblFullName.Size     = new System.Drawing.Size(120, 23);
-            txtFullName.Location = new System.Drawing.Point(controlX, row);
+            txtFullName.Location = new System.Drawing.Point(cx, row);
             txtFullName.Size     = new System.Drawing.Size(260, 23);
-            row += rowH;
-
-            // Giới tính
-            lblGender.Text     = "Giới tính:";
-            lblGender.Location = new System.Drawing.Point(labelX, row + 3);
-            lblGender.Size     = new System.Drawing.Size(120, 23);
-            rdoMale.Text       = "Nam";
-            rdoMale.Location   = new System.Drawing.Point(controlX, row);
-            rdoMale.Checked    = true;
-            rdoFemale.Text     = "Nữ";
-            rdoFemale.Location = new System.Drawing.Point(controlX + 80, row);
-            row += rowH;
+            row += rh;
 
             // Ngày sinh
             lblBirthDate.Text     = "Ngày sinh:";
-            lblBirthDate.Location = new System.Drawing.Point(labelX, row + 3);
+            lblBirthDate.Location = new System.Drawing.Point(lx, row + 3);
             lblBirthDate.Size     = new System.Drawing.Size(120, 23);
-            dtpBirthDate.Location = new System.Drawing.Point(controlX, row);
+            dtpBirthDate.Location = new System.Drawing.Point(cx, row);
             dtpBirthDate.Size     = new System.Drawing.Size(200, 23);
-            dtpBirthDate.Format   = System.Windows.Forms.DateTimePickerFormat.Short; // dd/MM/yyyy
-            row += rowH;
+            dtpBirthDate.Format   = System.Windows.Forms.DateTimePickerFormat.Short;
+            row += rh;
 
-            // Điện thoại (MaskedTextBox)
-            lblPhone.Text     = "Điện thoại:";
-            lblPhone.Location = new System.Drawing.Point(labelX, row + 3);
-            lblPhone.Size     = new System.Drawing.Size(120, 23);
-            mtxtPhone.Location = new System.Drawing.Point(controlX, row);
+            // Điện thoại
+            lblPhone.Text      = "Điện thoại:";
+            lblPhone.Location  = new System.Drawing.Point(lx, row + 3);
+            lblPhone.Size      = new System.Drawing.Size(120, 23);
+            mtxtPhone.Location = new System.Drawing.Point(cx, row);
             mtxtPhone.Size     = new System.Drawing.Size(200, 23);
-            mtxtPhone.Mask     = "(000) 000-0000";  // Mask SĐT
-            row += rowH;
+            mtxtPhone.Mask     = "(000) 000-0000";
+            row += rh;
 
-            // Khóa học (ComboBox)
-            lblCourse.Text     = "Khóa học:";
-            lblCourse.Location = new System.Drawing.Point(labelX, row + 3);
-            lblCourse.Size     = new System.Drawing.Size(120, 23);
-            cboCourse.Location    = new System.Drawing.Point(controlX, row);
-            cboCourse.Size        = new System.Drawing.Size(260, 23);
+            // Khóa học
+            lblCourse.Text      = "Khóa học:";
+            lblCourse.Location  = new System.Drawing.Point(lx, row + 3);
+            lblCourse.Size      = new System.Drawing.Size(120, 23);
+            cboCourse.Location  = new System.Drawing.Point(cx, row);
+            cboCourse.Size      = new System.Drawing.Size(260, 23);
             cboCourse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            row += rowH;
+            row += rh;
 
-            // Môn học thêm
-            lblExtras.Text     = "Môn bổ sung:";
-            lblExtras.Location = new System.Drawing.Point(labelX, row + 3);
-            lblExtras.Size     = new System.Drawing.Size(120, 23);
-            chkEnglish.Text    = "Tiếng Anh";
-            chkEnglish.Location= new System.Drawing.Point(controlX, row);
-            chkSoftSkills.Text = "Kỹ năng mềm";
-            chkSoftSkills.Location = new System.Drawing.Point(controlX + 110, row);
-            chkIT.Text         = "Tin học";
-            chkIT.Location     = new System.Drawing.Point(controlX + 230, row);
-            row += rowH + 10;
+            // Môn bổ sung
+            lblExtras.Text         = "Môn bổ sung:";
+            lblExtras.Location     = new System.Drawing.Point(lx, row + 3);
+            lblExtras.Size         = new System.Drawing.Size(120, 23);
+            chkEnglish.Text        = "Tiếng Anh";
+            chkEnglish.Location    = new System.Drawing.Point(cx, row);
+            chkEnglish.AutoSize    = true;
+            chkSoftSkills.Text     = "Kỹ năng mềm";
+            chkSoftSkills.Location = new System.Drawing.Point(cx + 105, row);
+            chkSoftSkills.AutoSize = true;
+            chkIT.Text             = "Tin học";
+            chkIT.Location         = new System.Drawing.Point(cx + 220, row);
+            chkIT.AutoSize         = true;
+            row += rh + 5;
 
-            // Buttons
+            // Nút Đăng Ký
             btnRegister.Text      = "Đăng Ký";
             btnRegister.Location  = new System.Drawing.Point(110, row);
             btnRegister.Size      = new System.Drawing.Size(110, 35);
@@ -116,6 +104,7 @@ namespace BT4_2_StudentRegistration
             btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             btnRegister.Click    += btnRegister_Click;
 
+            // Nút Xóa
             btnClear.Text      = "Xóa";
             btnClear.Location  = new System.Drawing.Point(250, row);
             btnClear.Size      = new System.Drawing.Size(110, 35);
@@ -125,8 +114,8 @@ namespace BT4_2_StudentRegistration
             btnClear.Click    += btnClear_Click;
 
             Controls.AddRange(new System.Windows.Forms.Control[] {
-                lblTitle, lblFullName, txtFullName,
-                lblGender, rdoMale, rdoFemale,
+                lblTitle,
+                lblFullName, txtFullName,
                 lblBirthDate, dtpBirthDate,
                 lblPhone, mtxtPhone,
                 lblCourse, cboCourse,
@@ -137,9 +126,9 @@ namespace BT4_2_StudentRegistration
             ResumeLayout(false);
         }
 
-        private System.Windows.Forms.Label          lblTitle, lblFullName, lblGender, lblBirthDate, lblPhone, lblCourse, lblExtras;
+        // Fields - đã xóa rdoMale, rdoFemale, lblGender
+        private System.Windows.Forms.Label          lblTitle, lblFullName, lblBirthDate, lblPhone, lblCourse, lblExtras;
         private System.Windows.Forms.TextBox        txtFullName;
-        private System.Windows.Forms.RadioButton    rdoMale, rdoFemale;
         private System.Windows.Forms.DateTimePicker dtpBirthDate;
         private System.Windows.Forms.MaskedTextBox  mtxtPhone;
         private System.Windows.Forms.ComboBox       cboCourse;
--- RegistrationForm.cs
+++ RegistrationForm.cs
@@ -24,46 +24,41 @@ namespace BT4_2_StudentRegistration
             };
 
             cboCourse.DataSource    = courses;
-            cboCourse.DisplayMember = "Name";   // Hiển thị tên khóa
-            cboCourse.ValueMember   = "Id";     // Giá trị là Id
+            cboCourse.DisplayMember = "Name";
+            cboCourse.ValueMember   = "Id";
         }
 
         // Nút Đăng Ký
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            // Thu thập giới tính
-            string gender = rdoMale.Checked ? "Nam" : rdoFemale.Checked ? "Nữ" : "Khác";
-
             // Thu thập môn học thêm (CheckBox)
             var extras = new System.Collections.Generic.List<string>();
-            if (chkEnglish.Checked)  extras.Add("Tiếng Anh");
+            if (chkEnglish.Checked)    extras.Add("Tiếng Anh");
             if (chkSoftSkills.Checked) extras.Add("Kỹ năng mềm");
-            if (chkIT.Checked)       extras.Add("Tin học văn phòng");
+            if (chkIT.Checked)         extras.Add("Tin học văn phòng");
             string extraStr = extras.Count > 0 ? string.Join(", ", extras) : "Không";
 
-            string info = $"===== THÔNG TIN ĐĂNG KÝ =====\n" +
-                          $"Họ tên:       {txtFullName.Text}\n" +
-                          $"Giới tính:    {gender}\n" +
-                          $"Ngày sinh:    {dtpBirthDate.Value:dd/MM/yyyy}\n" +
-                          $"Điện thoại:   {mtxtPhone.Text}\n" +
-                          $"Khóa học:     {cboCourse.Text}\n" +
-                          $"Môn bổ sung:  {extraStr}\n" +
-                          $"==============================";
+            string info = "===== THÔNG TIN ĐĂNG KÝ =====\n" +
+                          $"Họ tên:      {txtFullName.Text}\n" +
+                          $"Ngày sinh:   {dtpBirthDate.Value:dd/MM/yyyy}\n" +
+                          $"Điện thoại:  {mtxtPhone.Text}\n" +
+                          $"Khóa học:    {cboCourse.Text}\n" +
+                          $"Môn bổ sung: {extraStr}\n" +
+                          "==============================";
 
             MessageBox.Show(info, "Đăng Ký Thành Công", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        // Nút Hủy
+        // Nút Xóa form
         private void btnClear_Click(object sender, EventArgs e)
         {
             txtFullName.Clear();
-            rdoMale.Checked       = true;
-            dtpBirthDate.Value    = DateTime.Today;
+            dtpBirthDate.Value      = DateTime.Today;
             mtxtPhone.Clear();
             cboCourse.SelectedIndex = 0;
-            chkEnglish.Checked    = false;
-            chkSoftSkills.Checked = false;
-            chkIT.Checked         = false;
+            chkEnglish.Checked      = false;
+            chkSoftSkills.Checked   = false;
+            chkIT.Checked           = false;
         }
     }
 }
